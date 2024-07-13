054f9bc7f1c4151ab5b76616a31ef4765110a534
SYED SHAHID NAZEER <nazeershahid256@gmail.com>
2024-07-13 07:26:06 -0700

    homepage

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
+import os
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def main():
+    # Get the current working directory
+    current_dir = os.getcwd()
+
+    st.title("My Portfolio")
+    st.image(os.path.join(current_dir, "images", "1.jpg"), use_column_width=True)
+    st.write("Hi, I'm [Your Name]. I'm passionate about data science and machine learning.")
+    st.write("Studies:")
+    st.write("- [Your Degree], [Your University]")
+    st.write("Experiences:")
+    st.write("- [Your Previous Job 1]")
+    st.write("- [Your Previous Job 2]")
+
+if __name__ == "__main__":
+    main()
